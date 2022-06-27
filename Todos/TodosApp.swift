3fdd7ab6117f23047b3eb648eeda34391b9ed903
Oluwatobi Omotayo <tobitech@ymail.com>
2022-06-27 10:20:13 +0100

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Oluwatobi Omotayo on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
